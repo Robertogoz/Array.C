056b1feac1890a9051fb9e063a63739016f8efb6
Roberto Goz <63943375+Robertogoz@users.noreply.github.com>
2021-10-16 15:52:50 -0300

    Create ex03.c

---
+++ 2D Arrays/ex03.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void) {
+	int M[3][3], i, j;
+	
+	for (i=0;i<3;i++) {
+		for (j=0;j<3;j++) {
+			scanf(" %d", &M[i][j]);	
+		}
+	}
+	
+	for (i=0;i<3;i++) {
+		for (j=0;j<3;j++) {
+			printf("%d ", M[j][i]);	
+		}
+		printf("\n");
+	}
+}
